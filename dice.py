c984a795dc2980bbf127acb81339ee106928e093
Santiago Benalcázar <santiago.benalcazar@ppm.com.ec>
2017-05-08 18:01:39 -0500

    1

--- dice.py
+++ dice.py
@@ -1,1 +1,18 @@
+import sys
 
+n = raw_input()
+trial = raw_input()
+
+if int(n) >= 1 and int(n) <= 1000000:
+    cont = 0
+    aux = 0
+    for i in trial:
+        var = int(i)
+        if var == 6:
+            cont += 1
+        if cont > 3:
+            aux += 1
+            cont = 0
+    print aux
+else:
+    sys.exit()
